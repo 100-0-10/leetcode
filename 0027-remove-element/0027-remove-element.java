8ad17ff680cb6abb5a17ef954a3ff9dbae71706d
100_0_10 / 백영렬 <61173154+100-0-10@users.noreply.github.com>
2024-07-22 22:28:53 +0900

    Time: 1 ms (6.7%), Space: 42.1 MB (13.18%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                count++;
+                nums[i] = 51;
+            }
+        }
+        
+        Arrays.sort(nums);
+
+        return nums.length - count;
+    }
+}
